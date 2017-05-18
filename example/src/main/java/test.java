8834cc50ec21531bb6c3118d0a9a8e6b41bc1b0d
Anping Wang <15236759872@163.com>
2017-05-18 20:09:12 +0800

    first commit

---
+++ example/src/main/java/test.java
@@ -0,0 +1,21 @@
+/**
+ * Created by 未来人类 on 2017/5/17.
+ */
+public class test {
+    private int id ;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    @Override
+    public String toString() {
+        return "test{" +
+                "id=" + id +
+                '}';
+    }
+}
